753c724b22672de254bf10aad63cbc6a68384586
jang010505 <jang010505@inu.ac.kr>
2023-08-02 22:45:32 +0900

    feat: Upload backend project files

---
+++ website/src/main/kotlin/bbeb/website/WebsiteApplication.kt
@@ -0,0 +1,11 @@
+package bbeb.website
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebsiteApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebsiteApplication>(*args)
+}
