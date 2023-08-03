a728a7fc504cc6e223f81c07616ca1da21ce809b
jang010505 <jang010505@inu.ac.kr>
2023-08-03 22:49:34 +0900

    feat: #15 Init Swagger Page

---
+++ website/src/main/java/bbeb/website/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package bbeb.website.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("BBEB API DOCS")
+                        .description("BBEB Project API DOCS 입니다.")
+                        .version("v0.0.1"));
+    }
+}
