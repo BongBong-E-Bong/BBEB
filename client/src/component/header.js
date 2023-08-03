373f720b9a1dea5d8a1e5b6c34ed03a41a1cb92c
suyeon <moa0823@naver.com>
2023-08-03 20:12:01 +0900

    #10 헤더 레이아웃 구현

--- client/src/component/header.js
+++ client/src/component/header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Stack } from "@mui/material";
 import bbeblogo from "../image/bbeblogo.png";
+import profilephoto from "../image/profilephoto.png";
 import { useNavigate } from "react-router-dom";
 
 function Header() {
@@ -8,7 +9,7 @@ function Header() {
 
   return (
     <Stack
-      bgcolor="black"
+      bgcolor="white"
       width="100%"
       height="80px"
       direction="row"
@@ -19,18 +20,33 @@ function Header() {
         src={bbeblogo}
         alt="logo"
         width="150px"
-        height="55px"
-        style={{ cursor: "pointer", marginLeft: "30px" }}
+        height="50px"
+        style={{ cursor: "pointer", marginLeft: "60px" }}
         onClick={() => {
           navigate("/");
         }}
       />
-      <Stack
-        bgcolor="red"
-        width="30px"
-        height="30px"
-        style={{ marginRight: "30px" }}
-      ></Stack>
+      <Stack style={{ marginRight: "60px" }}>
+        {/* <Stack //로그인X
+          width="150px"
+          height="30px"
+          direction="row"
+          justifyContent="space-between"
+        >
+          <Stack style={{ cursor: "pointer" }}>로그인</Stack>
+          <Stack>|</Stack>
+          <Stack style={{ cursor: "pointer" }}>회원 가입</Stack>
+        </Stack> */}
+        <Stack>
+          <img
+            src={profilephoto}
+            alt="profilephoto"
+            width="60px"
+            height="60px"
+            style={{ cursor: "pointer" }}
+          />
+        </Stack>
+      </Stack>
     </Stack>
   );
 }
