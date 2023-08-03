be015abe7cf2ac4b243034173fa6fc8566519948
suyeon <moa0823@naver.com>
2023-08-03 19:53:59 +0900

    #10 로고 클릭 시 메인 페이지로 이동 구현

--- client/src/component/header.js
+++ client/src/component/header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Stack } from "@mui/material";
 import bbeblogo from "../image/bbeblogo.png";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
     <Stack
       bgcolor="black"
@@ -13,8 +15,22 @@ function Header() {
       justifyContent="space-between"
       alignItems="center"
     >
-      <img src={bbeblogo} alt="logo" width="150px" height="55px" />
-      <Stack bgcolor="red" width="30px" height="30px"></Stack>
+      <img
+        src={bbeblogo}
+        alt="logo"
+        width="150px"
+        height="55px"
+        style={{ cursor: "pointer", marginLeft: "30px" }}
+        onClick={() => {
+          navigate("/");
+        }}
+      />
+      <Stack
+        bgcolor="red"
+        width="30px"
+        height="30px"
+        style={{ marginRight: "30px" }}
+      ></Stack>
     </Stack>
   );
 }
