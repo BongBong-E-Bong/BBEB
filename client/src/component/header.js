e48f70eb6ee2627e83d3f4b831f34400c1742302
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2023-08-08 20:43:05 +0900

    #9 axios 살짝...?

--- client/src/component/header.js
+++ client/src/component/header.js
@@ -4,13 +4,10 @@ import { Stack, TextField } from "@mui/material";
 import bbeblogo from "../image/bbeblogo.png";
 import basicProfile from "../image/profilephoto.png";
 import { useNavigate } from "react-router-dom";
-import login from "../image/login.png";
-import kakaologo from "../image/kakaologo.png";
 import register from "../image/register.png";
+import axios from 'axios';
 
 function Header() {
-  const [showLoginForm, setShowLoginForm] = useState(false);
-  const [showRegisterForm, setShowRegisterForm] = useState(false);
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = React.useState(false);
   const menuOpen = () => {
@@ -29,6 +26,17 @@ function Header() {
       reader.readAsDataURL(e.target.files[0]);
     }
   };
+
+  const [showRegisterForm, setShowRegisterForm] = useState(false);
+
+  const RegisterForm = () => {
+    const [formData, setFormData] = useState({
+      loginId: 'string',
+      password: 'string',
+      nickname: 'string',
+      email: 'string',
+    });
+  }
 
   return (
     <>
@@ -126,57 +134,71 @@ function Header() {
             setShowRegisterForm(true); // Show the login form when clicked
           }}>회원가입</Stack>
         {showRegisterForm && (
-          <Stack position="fixed" width="1230px" height="617px"
-            display="flex" direction="row" alignItems="center"
-            justifyContent="space-around" bgcolor="#F88C8C" >
-            <Stack width="753px" height="598px" justifyContent="center" >
-              <img
-                src={register} alt="register icon"
-                width="607px" height="601px"
-              />
-            </Stack>
-            <Stack width="440px" height="598px" justifyContent="center" gap="40px">
-              <Stack fontSize="36px" display="flex" marginLeft="100px">Register</Stack>
-              <Stack gap="50px">
-                <TextField
-                  placeholder={"id를 입력하세요"}
-                  multiline
-                  maxRows={4}
-                  InputProps={{
-                    style: { backgroundColor: "white", borderRadius: "20px", width: "341px", height: "40px" }
-                  }}
-                />
-                <TextField
-                  placeholder={"비밀번호"}
-                  multiline
-                  maxRows={4}
-                  InputProps={{
-                    style: { backgroundColor: "white", borderRadius: "20px", width: "341px", height: "40px" }
-                  }}
-                />
-                <TextField
-                  placeholder={"비밀번호 다시 입력"}
-                  multiline
-                  maxRows={4}
-                  InputProps={{
-                    style: { backgroundColor: "white", borderRadius: "20px", width: "341px", height: "40px" }
-                  }}
-                />
-                <TextField
-                  placeholder={"닉네임"}
-                  multiline
-                  maxRows={4}
-                  InputProps={{
-                    style: { backgroundColor: "white", borderRadius: "20px", width: "341px", height: "40px" }
-                  }}
+          <Stack height="100%" alignItems="center" justifyContent="center">
+            <Stack position="fixed" width="1230px" height="617px"
+              display="flex" direction="row" alignItems="center"
+              justifyContent="space-around" bgcolor="#F88C8C" >
+              <Stack width="753px" height="598px" justifyContent="center" >
+                <img
+                  src={register} alt="register icon"
+                  width="607px" height="601px"
                 />
               </Stack>
-              <Stack
-                bgcolor="#D76464"
-                style={{ cursor: "pointer", color: "white", borderRadius: "20px", width: "341px", height: "33px", alignItems: "center", justifyContent: "center", boxShadow: "0px 3px 2px rgba(0, 0, 0, 0.3)" }}
-                onClick={() => {
-                  // 클릭 이벤트 처리 코드를 여기에 추가
-                }}>REGISTER</Stack>
+              <Stack width="440px" height="598px" justifyContent="center" gap="45px">
+                <Stack fontSize="36px" display="flex" marginLeft="100px">Register</Stack>
+                <Stack gap="45px">
+                  <TextField
+                    name="loginId"
+                    placeholder={"id를 입력하세요"}
+                    multiline
+                    maxRows={4}
+                    InputProps={{
+                      style: { backgroundColor: "white", borderRadius: "20px", width: "341px", height: "40px" }
+                    }}
+                  />
+                  <TextField
+                    name="password"
+                    placeholder={"비밀번호"}
+                    multiline
+                    maxRows={4}
+                    InputProps={{
+                      style: { backgroundColor: "white", borderRadius: "20px", width: "341px", height: "40px" }
+                    }}
+                  />
+                  <TextField
+                    placeholder={"비밀번호 다시 입력"}
+                    multiline
+                    maxRows={4}
+                    InputProps={{
+                      style: { backgroundColor: "white", borderRadius: "20px", width: "341px", height: "40px" }
+                    }}
+                  />
+                  <TextField
+                    name="nickname"
+                    placeholder={"닉네임"}
+                    multiline
+                    maxRows={4}
+                    InputProps={{
+                      style: { backgroundColor: "white", borderRadius: "20px", width: "341px", height: "40px" }
+                    }}
+                  />
+                  <TextField
+                    name="email"
+                    placeholder={"이메일"}
+                    multiline
+                    maxRows={4}
+                    InputProps={{
+                      style: { backgroundColor: "white", borderRadius: "20px", width: "341px", height: "40px" }
+                    }}
+                  />
+                </Stack>
+                <Stack
+                  bgcolor="#D76464"
+                  style={{ cursor: "pointer", color: "white", borderRadius: "20px", width: "341px", height: "33px", alignItems: "center", justifyContent: "center", boxShadow: "0px 3px 2px rgba(0, 0, 0, 0.3)" }}
+                  onClick={() => {
+                    // 클릭 이벤트 처리 코드를 여기에 추가
+                  }}>REGISTER</Stack>
+              </Stack>
             </Stack>
           </Stack>
         )}
@@ -297,5 +319,6 @@ function Header() {
     </>
   );
 }
+
 
 export default Header;
