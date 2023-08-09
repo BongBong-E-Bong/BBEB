fc8624ee8f56f3b368b8630e0a9587403f9e9f95
suyeon <moa0823@naver.com>
2023-08-09 17:51:56 +0900

    #18 게시글 레이아웃 구현

--- client/src/page/Board/post.js
+++ client/src/page/Board/post.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Header from "../../component/header";
-import { Stack } from "@mui/material";
+import { Stack, Chip } from "@mui/material";
+import obong from "../../image/obong.png";
+import basicProfile from "../../image/profilephoto.png";
+// import VisibilityIcon from "@mui/icons-material/Visibility"; 아이콘 설치
 
 function Post() {
   return (
@@ -8,15 +11,104 @@ function Post() {
       <Header />
       <Stack
         minHeight="100vh"
-        height="100vh"
+        height="fit-content"
         width="100%"
-        justifyContent="center"
         alignItems="center"
-        paddingTop="100px"
+        gap="40px"
       >
-        <Stack width="373px" height="28px" bgcolor="#FFDEDE"></Stack>
-        <Stack width="1314px" minHeight="800px" bgcolor="#FAF3F0"></Stack>
+        <Stack width="1314px" justifyContent="flex-end">
+          <Stack
+            width="400px"
+            height="28px"
+            bgcolor="#FFDEDE"
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            gap="10px"
+            marginTop="150px"
+            style={{ cursor: "pointer" }}
+          >
+            <Stack style={{ fontSize: "50px" }}>오봉이의 게시판</Stack>
+            <img src={obong} alt="obong" width="75px" height="85px"></img>
+          </Stack>
+        </Stack>
+
+        <Stack
+          width="1314px"
+          minHeight="700px"
+          height="fit-content"
+          bgcolor="#FAF3F0"
+          gap="15px"
+        >
+          <Stack //제목
+            margin="80px 150px 0 150px"
+            style={{ fontSize: "50px", fontWeight: "bold" }}
+          >
+            title
+          </Stack>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            margin="0 150px 0 150px"
+            alignItems="center"
+          >
+            <Stack direction="row" gap="10px">
+              <img //프사
+                src={basicProfile}
+                alt="basicProfile"
+                width="60px"
+                height="60px"
+              ></img>
+              <Stack gap="2px" justifyContent="center" alignItems="center">
+                {/* 닉네임  */}
+                <Stack style={{ fontSize: "20px" }}>nickname</Stack>
+                {/* 날짜 */}
+                <Stack style={{ fontSize: "17px" }}>2001.08.23</Stack>
+              </Stack>
+              {/* 태그 */}
+              <Stack
+                direction="row"
+                gap="10px"
+                marginLeft="10px"
+                alignItems="center"
+              >
+                <Chip label="이봉이 좋아" variant="outlined" />
+                <Chip label="이봉이 좋아" variant="outlined" />
+              </Stack>
+            </Stack>
+            <Stack direction="row" gap="15px">
+              <Stack style={{ fontSize: "20px", cursor: "pointer" }}>
+                수정
+              </Stack>
+              <Stack style={{ fontSize: "20px" }}>|</Stack>
+              <Stack style={{ fontSize: "20px", cursor: "pointer" }}>
+                삭제
+              </Stack>
+              <Stack>
+                {/* 조회수 */}
+                {/* <VisibilityIcon></VisibilityIcon> */}
+                <Stack style={{ fontSize: "20px" }}>8</Stack>
+              </Stack>
+            </Stack>
+          </Stack>
+          <Stack alignItems="center">
+            <Stack
+              width="995px"
+              minHeight="450px"
+              bgcolor="white"
+              height="fit-content"
+              marginBottom="30px"
+            >
+              <Stack margin="70px 140px 70px 140px">
+                <Stack bgcolor="black" width="30px" height="2000px">
+                  hihihi
+                </Stack>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Stack>
       </Stack>
+      <Stack>댓글</Stack>
     </>
   );
 }
