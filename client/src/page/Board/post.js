d154cd8edcd338434ec59f282d06fa1ee9639293
suyeon <moa0823@naver.com>
2023-08-13 01:55:03 +0900

    #36 게시글 px=>비율로 수정

--- client/src/page/Board/post.js
+++ client/src/page/Board/post.js
@@ -17,28 +17,12 @@ function Post() {
         height="fit-content"
         width="100%"
         alignItems="center"
-        gap="40px"
       >
-        {/* <Stack width="100%" height="5%" marginLeft="10%" marginTop="10%">
+        <Stack width="100%" height="22vh">
           <Stack
-            width="20%"
-            height="100%"
-            bgcolor="#FFDEDE"
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-            // gap="10px"
-            style={{ cursor: "pointer" }}
-          >
-            <Stack style={{ fontSize: "200%" }}>오봉이의 게시판</Stack>
-            <img src={obong} alt="obong" width="10%" height="10%"></img>
-          </Stack>
-        </Stack> */}
-        <Stack width="100%" height="30vh" bgcolor="black">
-          <Stack
-            width="25%"
+            width="21%"
             height="10%"
-            marginLeft="10%"
+            marginLeft="15%"
             marginTop="10%"
             bgcolor="#FFDEDE"
             direction="row"
@@ -46,44 +30,50 @@ function Post() {
             alignItems="center"
             style={{ cursor: "pointer" }}
           >
-            <Stack width="100%" height="100%" style={{ fontSize: "200%" }}>
+            <Stack
+              width="100%"
+              height="100%"
+              style={{ fontSize: "40px" }}
+              alignItems="center"
+              justifyContent="center"
+            >
               오봉이의 게시판
             </Stack>
-            <img src={obong} alt="obong" width="10%" height="10%"></img>
+            <img src={obong} alt="obong" width="20%" height="400%"></img>
           </Stack>
         </Stack>
 
         <Stack
-          width="1314px"
-          minHeight="700px"
+          width="70%"
+          marginTop="2%"
+          minHeight="22vh" //72-
           height="fit-content"
           bgcolor="#FAF3F0"
-          gap="15px"
         >
           <Stack //제목
-            margin="80px 150px 0 150px"
-            style={{ fontSize: "50px", fontWeight: "bold" }}
+            margin="6% 10% 0.6% 10%"
+            style={{ fontSize: "40px", fontWeight: "bold" }}
           >
             title
           </Stack>
           <Stack
             direction="row"
             justifyContent="space-between"
-            margin="0 150px 0 150px"
+            margin="0 10% 1% 10%"
             alignItems="center"
           >
             <Stack direction="row" gap="10px">
               <img //프사
                 src={basicProfile}
                 alt="basicProfile"
-                width="60px"
-                height="60px"
+                width="8%"
+                height="8%"
               ></img>
               <Stack gap="2px" justifyContent="center" alignItems="center">
                 {/* 닉네임  */}
-                <Stack style={{ fontSize: "20px" }}>nickname</Stack>
+                <Stack style={{ fontSize: "17px" }}>nickname</Stack>
                 {/* 날짜 */}
-                <Stack style={{ fontSize: "17px" }}>2001.08.23</Stack>
+                <Stack style={{ fontSize: "14px" }}>2001.08.23</Stack>
               </Stack>
               {/* 태그 */}
               <Stack
@@ -98,59 +88,81 @@ function Post() {
                       label={tag}
                       variant="outlined"
                       color="primary"
-                      style={{ cursor: "pointer" }}
+                      style={{ cursor: "pointer", fontSize: "15px" }}
                     />
                   );
                 })}
               </Stack>
             </Stack>
-            <Stack direction="row" gap="15px" marginRight="20px">
+            <Stack direction="row" gap="15px" width="20%">
               {/* 수정 삭제 버튼 글쓴이가 아닐 경우 */}
-              <Stack style={{ fontSize: "20px", cursor: "pointer" }}>
+              <Stack style={{ fontSize: "17px", cursor: "pointer" }}>
                 수정
               </Stack>
-              <Stack style={{ fontSize: "20px" }}>|</Stack>
-              <Stack style={{ fontSize: "20px", cursor: "pointer" }}>
+              <Stack style={{ fontSize: "17px" }}>|</Stack>
+              <Stack style={{ fontSize: "17px", cursor: "pointer" }}>
                 삭제
               </Stack>
               <Stack direction="row" gap="10px">
-                <VisibilityIcon style={{ color: "#767676" }}></VisibilityIcon>
-                <Stack style={{ fontSize: "20px" }}>8</Stack>
-              </Stack>
-            </Stack>
-          </Stack>
-          <Stack alignItems="center">
-            <Stack
-              width="995px"
-              minHeight="450px"
-              bgcolor="white"
-              height="fit-content"
-              marginBottom="30px"
-            >
-              <Stack margin="70px 140px 40px 140px" alignItems="center">
-                {/* content */}
-                <Stack bgcolor="gray" width="300px" height="2000px">
-                  content
-                </Stack>
-                <Stack
-                  marginTop="60px"
-                  direction="row"
-                  alignItems="center"
-                  gap="20px"
-                >
-                  <ThumbUpIcon
-                    fontSize="large"
-                    style={{ cursor: "pointer", color: "#767676" }}
-                  ></ThumbUpIcon>
-                  {/* 따봉수 */}
-                  <Stack style={{ fontSize: "30px" }}>2</Stack>
-                </Stack>
+                <VisibilityIcon
+                  style={{ color: "#767676" }}
+                  sx={{ fontSize: "17px" }}
+                ></VisibilityIcon>
+                <Stack style={{ fontSize: "17px" }}>8</Stack>
               </Stack>
             </Stack>
           </Stack>
         </Stack>
+        <Stack
+          width="70%"
+          minHeight="50.5vh"
+          height="fit-content"
+          bgcolor="#FAF3F0"
+          // justifyContent="center"
+          alignItems="center"
+        >
+          <Stack
+            width="80%"
+            minHeight="48vh"
+            height="fit-content"
+            bgcolor="white"
+            justifyContent="space-between"
+            alignItems="center"
+            style={{ borderRadius: "30px" }}
+          >
+            <Stack
+              width="75%"
+              minHeight="18.7vh"
+              height="fit-content"
+              // bgcolor="white"
+              margin="10% 0 10% 0"
+              justifyContent="center"
+              alignItems="center"
+              bgcolor="white"
+            >
+              hihihihi
+              {/* content */}
+            </Stack>
+            <Stack
+              direction="row"
+              justifyContent="center"
+              gap="4%"
+              width="100%"
+              height="10vh"
+              bgcolor="white"
+              style={{ borderRadius: "30px" }}
+            >
+              <ThumbUpIcon
+                fontSize="large"
+                style={{ cursor: "pointer", color: "#767676" }}
+              ></ThumbUpIcon>
+              {/* 따봉수 */}
+              <Stack style={{ fontSize: "30px" }}>2</Stack>
+            </Stack>
+          </Stack>
+        </Stack>
       </Stack>
-      <Stack>댓글</Stack>
+      {/* <Stack>댓글</Stack> */}
     </>
   );
 }
