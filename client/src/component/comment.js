f6c81e4b41b88fd9af4728ab16373023ea23f36c
suyeon <moa0823@naver.com>
2023-08-13 20:20:09 +0900

    #35 댓글 컴포넌트 틀 잡기

---
+++ client/src/component/comment.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+function Comment() {
+  return (
+    <Stack
+      minHeight="12vh"
+      height="fit-content"
+      weight="100%"
+      bgcolor="white"
+      alignItems="center"
+    >
+      <Stack bgcolor="#FAF3F0" width="70%" height="12vh"></Stack>
+    </Stack>
+  );
+}
+
+export default Comment;
