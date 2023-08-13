14774ad88a2f132c22f199aff659e6bcd3bc0112
jang010505 <jang010505@inu.ac.kr>
2023-08-14 03:03:01 +0900

    CICD

--- website/src/main/java/bbeb/website/WebsiteApplication.java
+++ website/src/main/java/bbeb/website/WebsiteApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 public class WebsiteApplication {
 
-
 	public static void main(String[] args) {
 		SpringApplication.run(WebsiteApplication.class, args);
 	}
