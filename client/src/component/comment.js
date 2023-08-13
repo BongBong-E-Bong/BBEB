7ec417f13b8a95ebc4b0659d9d174820c8ce670e
suyeon <moa0823@naver.com>
2023-08-13 21:02:57 +0900

    #35 댓글 입력창 구현

--- client/src/component/comment.js
+++ client/src/component/comment.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Stack } from "@mui/material";
+import { Stack, TextField, Button } from "@mui/material";
+import basicProfile from "../image/profilephoto.png";
 
 function Comment() {
   return (
@@ -10,7 +11,41 @@ function Comment() {
       bgcolor="white"
       alignItems="center"
     >
-      <Stack bgcolor="#FAF3F0" width="70%" height="12vh"></Stack>
+      <Stack bgcolor="#FAF3F0" width="70%" height="12vh">
+        <Stack
+          width="100%"
+          height="100%"
+          justifyContent="center"
+          alignItems="center"
+          direction="row"
+        >
+          <img
+            alt="basicProfile"
+            src={basicProfile}
+            width="6.5%"
+            height="70%"
+            style={{ borderRadius: "50%" }}
+          />
+          <Stack width="65%">
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              color="primary"
+              sx={{ bgcolor: "white" }}
+              placeholder="댓글을 입력하세요."
+            />
+          </Stack>
+          <Button variant="contained" color="primary" sx={{ height: "60%" }}>
+            <Stack>댓글 쓰기</Stack>
+            <Stack direction="row">
+              <Stack>(</Stack>
+              <Stack>3</Stack>
+              {/* 댓글수 */}
+              <Stack>)</Stack>
+            </Stack>
+          </Button>
+        </Stack>
+      </Stack>
     </Stack>
   );
 }
