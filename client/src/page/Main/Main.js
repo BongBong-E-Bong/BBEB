59727467668f4a2206e3b2c6822c91d1b25d231d
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2023-08-15 16:35:05 +0900

    #9 회원가입 레이아웃 구성

--- client/src/page/Main/Main.js
+++ client/src/page/Main/Main.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Header from "../../component/header";
 import { Stack } from "@mui/material";
+import Register from "../../component/register";
 
 function Main() {
   return (
     <Stack minHeight="100vh" height="100vh" weight="100%">
-      <Header />
+      <Register />
     </Stack>
   );
 }
