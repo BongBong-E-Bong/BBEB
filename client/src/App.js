577411fb3a361e3dfa93b9586e3525924b6fb4fc
suyeon <moa0823@naver.com>
2023-08-18 16:58:09 +0900

    #54 이봉이 프로필 라우터 설정

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "../src/page/Main/Main.js";
 import Profile from "../src/page/Profile/Profile.js";
+import Twobong from "../src/page/Profile/Twobong.js";
 import Ranking from "../src/page/Ranking/Ranking.js";
 import Vote from "../src/page/Vote/Vote.js";
 import Post from "./page/Board/post.js";
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/twobong" element={<Twobong />} />
         <Route path="/ranking" element={<Ranking />} />
         <Route path="/vote" element={<Vote />} />
         <Route path="/post" element={<Post />} />
---
+++ client/src/page/Profile/Twobong.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+function Twobong() {
+  return (
+    <Stack minHeight="100vh" height="100vh" weight="100%" bgcolor="black" />
+  );
+}
+
+export default Twobong;
