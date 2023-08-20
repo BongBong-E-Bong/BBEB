80198770a8bea8861eb724b96c291846d9ce6d29
suyeon <moa0823@naver.com>
2023-08-20 19:51:56 +0900

    #54 일봉이 프로필 라우터 설정

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Main from "../src/page/Main/Main.js";
 import Profile from "../src/page/Profile/Profile.js";
-import ProfileForm from "./component/ProfileForm.js";
+import Onebong from "../src/page/Profile/Onebong.js";
 import Ranking from "../src/page/Ranking/Ranking.js";
 import Vote from "../src/page/Vote/Vote.js";
 import Post from "./page/Board/post.js";
@@ -13,7 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/profile" element={<Profile />} />
-        <Route path="/ProfileForm" element={<ProfileForm />} />
+        <Route path="/onebong" element={<Onebong />} />
         <Route path="/ranking" element={<Ranking />} />
         <Route path="/vote" element={<Vote />} />
         <Route path="/post" element={<Post />} />
---
+++ client/src/page/Profile/Onebong.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Stack } from "@mui/material";
+
+function Onebong() {
+  return (
+    <Stack minHeight="100vh" height="100vh" weight="100%" bgcolor="black" />
+  );
+}
+
+export default Onebong;
