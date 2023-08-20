df02048fca317085539a4c948eda66f71585af78
suyeon <moa0823@naver.com>
2023-08-20 20:37:03 +0900

    #54 클릭 시 스크롤 자동 이동 기능 추가

--- client/src/page/Profile/Onebong.js
+++ client/src/page/Profile/Onebong.js
@@ -4,14 +4,31 @@ import Header from "../../component/header";
 import ProfileForm from "../../component/ProfileForm";
 
 function Onebong() {
+  const scrollRef = React.useRef(null);
+
+  const scrollClick = () => {
+    scrollRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
       <Header />
-      <ProfileForm>hihi</ProfileForm>
+      <ProfileForm>
+        <Stack
+          width="50%"
+          height="1000px"
+          bgcolor="gray"
+          onClick={scrollClick}
+        ></Stack>
+        <Stack width="50%" height="1000px" bgcolor="pink"></Stack>
+        <Stack ref={scrollRef}></Stack>
+      </ProfileForm>
+      <Stack>
+        <Stack>일봉이 소개</Stack>
+        <Stack>일봉이 tmi</Stack>
+        <Stack>댓글</Stack>
+      </Stack>
     </>
-    // <Stack minHeight="100vh" height="100vh" weight="100%" bgcolor="black">
-
-    // </Stack>
   );
 }
 
