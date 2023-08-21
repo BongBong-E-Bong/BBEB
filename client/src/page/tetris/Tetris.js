c7a90390131cccbd418f0d981ab0807456e0bd39
sojeong0302 <01duqrl@naver.com>
2023-08-21 13:54:23 +0900

    # 테트리스 수정중

--- client/src/page/tetris/Tetris.js
+++ client/src/page/tetris/Tetris.js
@@ -1,9 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Tetris.css';
 import { TETROMINOS, randomTetromino } from './tetrominos';
 
 const Tetris = () => {
   const [tetromino, setTetromino] = useState(randomTetromino());
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const moveDown = () => {
+    setPosition((prevPosition) => ({ ...prevPosition, y: prevPosition.y + 1 }));
+  };
+
+  useEffect(() => {
+    const timer = setInterval(moveDown, 1000);
+    return () => clearInterval(timer);
+  }, []);
 
   return (
     <div className="tetris">
@@ -11,11 +21,17 @@ const Tetris = () => {
         {Array.from({ length: 20 }).map((_, y) => (
           <div key={y} className="row">
             {Array.from({ length: 10 }).map((_, x) => {
-              const cellValue = tetromino[y] && tetromino[y][x];
+              const tetrominoShape = tetromino && tetromino.shape;
+              const offsetY = y - position.y;
+              const offsetX = x - position.x;
+              const isTetrominoCell =
+                tetrominoShape &&
+                tetrominoShape[offsetY] &&
+                tetrominoShape[offsetY][offsetX];
               return (
                 <div
                   key={x}
-                  className={`cell ${cellValue ? 'tetromino' : ''}`}
+                  className={`cell ${isTetrominoCell ? "tetromino" : ""}`}
                 />
               );
             })}
@@ -25,5 +41,3 @@ const Tetris = () => {
     </div>
   );
 };
-
-export default Tetris;
--- client/src/page/tetris/tetrominos.js
+++ client/src/page/tetris/tetrominos.js
@@ -1,41 +1,40 @@
 export const TETROMINOS = {
-    // Define tetromino shapes
-    I: [
-      [0, 0, 0, 0],
-      [1, 1, 1, 1],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-    ],
-    J: [
-      [0, 0, 0],
-      [2, 2, 2],
-      [0, 0, 2],
-    ],
-    T: [
-      [0, 2, 0],
-      [2, 2, 2],
-      [0, 0, 0],
-    ],
-    O: [
-      [4, 4],
-      [4, 4],
-    ],
-    S: [
-      [0, 5, 5],
-      [5, 5, 0],
-      [0, 0, 0],
-    ],
-    Z: [
-      [6, 6, 0],
-      [0, 6, 6],
-      [0, 0, 0],
-    ],
-  };
+  // Define tetromino shapes
+  I: [
+    [1, 1, 1, 1],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ],
+  J: [
+    [2, 2, 2],
+    [0, 0, 2],
+    [0, 0, 0],
+  ],
+  T: [
+    [0, 2, 0],
+    [2, 2, 2],
+    [0, 0, 0],
+  ],
+  O: [
+    [4, 4],
+    [4, 4],
+  ],
+  S: [
+    [0, 5, 5],
+    [5, 5, 0],
+    [0, 0, 0],
+  ],
+  Z: [
+    [6, 6, 0],
+    [0, 6, 6],
+    [0, 0, 0],
+  ],
+};
 
-  export const randomTetromino = () => {
-    const tetrominos = "IJLOSTZ";
-    const randomTetromino =
-      tetrominos[Math.floor(Math.random() * tetrominos.length)];
-    return TETROMINOS[randomTetromino];
-  };
-  
+export const randomTetromino = () => {
+  const tetrominos = "IJLOSTZ";
+  const randomTetromino =
+    tetrominos[Math.floor(Math.random() * tetrominos.length)];
+  return TETROMINOS[randomTetromino];
+};
