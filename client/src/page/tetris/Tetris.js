954ed7fd505afa51bf5d6ef3ad7ab11e48305026
sojeong0302 <01duqrl@naver.com>
2023-08-22 15:53:15 +0900

    #20 바닥에 안닿는 문제 해결

--- client/src/page/tetris/Tetris.js
+++ client/src/page/tetris/Tetris.js
@@ -39,14 +39,13 @@ const Tetris = () => {
       updateGrid(currentTetromino, position.x, position.y, grid);
       setCurrentTetromino(randomTetromino());
       setPosition((prevPosition) => ({ ...prevPosition, y: 0 }));
-    } else if (
-      newY > 18 ||
-      checkCollision(currentTetromino, position.x, newY + 1, grid)
-    ) {
+      return;
+    } else if (newY > 18 || checkCollision(currentTetromino, position.x, newY + 1, grid)) {
       // 블럭이 더 이상 아래로 움직일 수 없으면 고정합니다.
       updateGrid(currentTetromino, position.x, newY, grid);
       setCurrentTetromino(randomTetromino());
       setPosition((prevPosition) => ({ ...prevPosition, y: 0 }));
+      return;
     } else {
       // 블럭을 아래로 이동시킵니다.
       setPosition((prevPosition) => ({ ...prevPosition, y: newY }));
@@ -58,7 +57,7 @@ const Tetris = () => {
       for (let col = 0; col < tetromino[row].length; col++) {
         const newY = y + row;
         const newX = x + col;
-  
+
         if (
           newY >= grid.length ||
           newX < 0 ||
@@ -98,7 +97,6 @@ const Tetris = () => {
       window.removeEventListener("keydown", handleKeyPress);
     };
   }, [currentTetromino, position.x, position.y]);
-  
 
   return (
     <div className="tetris">
@@ -109,7 +107,6 @@ const Tetris = () => {
               const tetrominoRow = y - position.y;
               const tetrominoCol = x - position.x;
 
-              // 아래에 이 조건문을 추가하여 블럭을 올바른 위치에 그리도록 합니다.
               const isTetrominoCell =
                 tetrominoRow >= 0 &&
                 tetrominoRow < currentTetromino.length &&
@@ -137,3 +134,5 @@ const Tetris = () => {
 };
 
 export default Tetris;
+
+// 나머지 코드는 동일하게 유지합니다.
