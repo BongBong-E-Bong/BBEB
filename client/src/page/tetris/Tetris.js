b99d51aeaf7e431db7c346a5930779e8ec8a085a
sojeong0302 <01duqrl@naver.com>
2023-08-22 20:40:53 +0900

    #20 게임오버 기능 구현

--- client/src/page/tetris/Tetris.js
+++ client/src/page/tetris/Tetris.js
@@ -8,6 +8,8 @@ const Tetris = () => {
   const [grid, setGrid] = useState(
     Array.from({ length: 20 }, () => Array(10).fill(0))
   );
+  const [gameOver, setGameOver] = useState(false);
+
 
   const handleKeyPress = (event) => {
     if (event.key === "ArrowLeft") {
@@ -33,24 +35,28 @@ const Tetris = () => {
 
   const moveDown = () => {
     const newY = position.y + 1;
-
+  
     if (checkCollision(currentTetromino, position.x, newY, grid)) {
-      // 현재 블럭을 고정하고 새로운 블럭을 생성합니다.
-      updateGrid(currentTetromino, position.x, position.y, grid);
-      setCurrentTetromino(randomTetromino());
-      setPosition((prevPosition) => ({ ...prevPosition, y: 0 }));
-      return;
+      if (position.y === 0) {
+        console.log("게임 오버!");
+        setGameOver(true);
+      } else {
+        // 현재 블럭을 고정하고 새로운 블럭을 생성합니다.
+        updateGrid(currentTetromino, position.x, position.y, grid);
+        setCurrentTetromino(randomTetromino());
+        setPosition((prevPosition) => ({ ...prevPosition, y: 0 }));
+      }
     } else if (newY > 18 || checkCollision(currentTetromino, position.x, newY + 1, grid)) {
       // 블럭이 더 이상 아래로 움직일 수 없으면 고정합니다.
       updateGrid(currentTetromino, position.x, newY, grid);
       setCurrentTetromino(randomTetromino());
       setPosition((prevPosition) => ({ ...prevPosition, y: 0 }));
-      return;
     } else {
       // 블럭을 아래로 이동시킵니다.
       setPosition((prevPosition) => ({ ...prevPosition, y: newY }));
     }
   };
+  
 
   const checkCollision = (tetromino, x, y, grid) => {
     for (let row = 0; row < tetromino.length; row++) {
@@ -73,21 +79,30 @@ const Tetris = () => {
 
   const updateGrid = (tetromino, x, y, grid) => {
     const newGrid = grid.map((row) => [...row]);
-
+  
     for (let row = 0; row < tetromino.length; row++) {
       for (let col = 0; col < tetromino[row].length; col++) {
         if (tetromino[row][col] !== 0) {
           const newY = y + row;
           const newX = x + col;
           newGrid[newY][newX] = tetromino[row][col];
+  
+          // Check if the tetromino has reached the top of the grid
+          if (newY <= 0) {
+            // Set the entire row to 0 when tetromino reaches the top
+            newGrid[newY].fill(0);
+            setGameOver(true);
+            return;
+          }
         }
       }
     }
-
+  
     setGrid(newGrid);
   };
+  
 
-  const moveDownInterval = 100; // 1초 (밀리초 단위)
+  const moveDownInterval = 10; // 1초 (밀리초 단위)
   useEffect(() => {
     const timer = setInterval(moveDown, moveDownInterval);
     window.addEventListener("keydown", handleKeyPress);
@@ -98,41 +113,66 @@ const Tetris = () => {
     };
   }, [currentTetromino, position.x, position.y]);
 
-  return (
-    <div className="tetris">
-      <div className="playground">
-        {grid.map((row, y) => (
-          <div key={y} className="row">
-            {row.map((cellValue, x) => {
-              const tetrominoRow = y - position.y;
-              const tetrominoCol = x - position.x;
+  const restartGame = () => {
+    setGrid(Array.from({ length: 20 }, () => Array(10).fill(0)));
+    setCurrentTetromino(randomTetromino());
+    setPosition({ x: 4, y: 0 });
+    setGameOver(false);
+  };
+  
+// ... (이전 코드 생략)
 
-              const isTetrominoCell =
-                tetrominoRow >= 0 &&
-                tetrominoRow < currentTetromino.length &&
-                tetrominoCol >= 0 &&
-                tetrominoCol < currentTetromino[tetrominoRow].length;
+return (
+  <div className="tetris">
+    <div className={`playground ${gameOver ? 'game-over' : ''}`}>
+      {grid.map((row, y) => (
+        <div key={y} className="row">
+          {row.map((cellValue, x) => {
+            const tetrominoRow = y - position.y;
+            const tetrominoCol = x - position.x;
 
-              const cellTetromino =
-                isTetrominoCell &&
-                currentTetromino[tetrominoRow][tetrominoCol];
+            const isTetrominoCell =
+              tetrominoRow >= 0 &&
+              tetrominoRow < currentTetromino.length &&
+              tetrominoCol >= 0 &&
+              tetrominoCol < currentTetromino[tetrominoRow].length;
 
+            const cellTetromino =
+              isTetrominoCell &&
+              currentTetromino[tetrominoRow][tetrominoCol];
+
+            // Set cells above the visible grid as empty cells
+            if (tetrominoRow < 0) {
               return (
                 <div
                   key={x}
-                  className={`cell ${
-                    cellValue !== 0 || cellTetromino ? "tetromino" : ""
-                  }`}
+                  className={`cell ${cellValue !== 0 ? "tetromino" : ""}`}
                 />
               );
-            })}
-          </div>
-        ))}
-      </div>
+            }
+
+            return (
+              <div
+                key={x}
+                className={`cell ${
+                  cellValue !== 0 || cellTetromino ? "tetromino" : ""
+                }`}
+              />
+            );
+          })}
+         </div>
+      ))}
     </div>
-  );
+    {gameOver && (
+      <div className="game-over">
+        <h1>게임 오버!</h1>
+        <button className="restart-button" onClick={restartGame}>
+          다시 시작
+        </button>
+      </div>
+    )}
+  </div>
+);
 };
 
 export default Tetris;
-
-// 나머지 코드는 동일하게 유지합니다.
