367412c04d3698cc8e04e04797d1d2620234df3e
sojeong0302 <01duqrl@naver.com>
2023-08-30 21:17:11 +0900

    #74 다른거 먼저...ㅜㅜ

--- client/src/page/Write/writeModal.js
+++ client/src/page/Write/writeModal.js
@@ -27,15 +27,146 @@ function WriteModal({ setOpen }) {
           justifyContent="center"
           width="47%"
           height="95%"
-          bgcolor="black"
-        ></Stack>
+
+        >
+          <Stack
+            borderTop="1px solid black"
+            borderBottom="1px solid black"
+            spacing={2}
+          >
+            <Stack></Stack>
+            <Stack fontSize="36px" alignItems="center">
+              Thumbnail upload
+            </Stack>
+            <Stack
+              bgcolor="#FFFBEE"
+              alignItems="center"
+              justifyContent="space-around"
+              fontSize="36px"
+              width="350px"
+              height="200px"
+              spacing={-10}
+            >
+              <Stack>안녕 난 오봉이야</Stack>
+              <Stack>게시판 규칙을 설명해줄게</Stack>
+            </Stack>
+            <Stack alignItems="center">
+              <img
+                src={writepoto}
+                alt="writeouto"
+                width="40px"
+                height="40px"
+              ></img>
+            </Stack>
+            <Stack></Stack>
+          </Stack>
+        </Stack>
         <Stack
           bgcolor="#fff"
           alignItems="center"
           justifyContent="center"
           width="47%"
           height="95%"
-        ></Stack>
+        ><Stack
+        sx={{
+          cursor: "pointer",
+          color: "black",
+          marginLeft: "390px",
+          marginTop: "-40px",
+        }}
+        onClick={handleCloseModal}
+      >
+        <Stack fontSize="40px">x</Stack>
+      </Stack>
+      <Stack
+        direction="row"
+        marginLeft="300px"
+        width="500px"
+        spacing={1}
+        marginTop="30px"
+      >
+        <Stack spacing={2}>
+          <Stack>🐷오봉이</Stack>
+          <Stack
+            fontSize="13px"
+            bgcolor="#FAF3F0"
+            width="200px"
+            height="30px"
+            alignItems="center"
+            justifyContent="center"
+            borderRadius="40px"
+          >
+            게시판 규칙은 확인 했니?
+          </Stack>
+        </Stack>
+        <Stack>
+          <img src={obong} alt="obong" width="20%" height="400%"></img>
+        </Stack>
+      </Stack>
+      <Stack fontSize="16px">
+        <ul>
+          <li style={{ marginBottom: "10px" }}>욕설을 사용하지 않는다.</li>
+          <li style={{ marginBottom: "10px" }}>
+            다른 사람을 비난하지 않는다.
+          </li>
+          <li style={{ marginBottom: "10px" }}>
+            음란성 게시물을 올리지 않는다.
+          </li>
+          <li style={{ marginBottom: "10px" }}>
+            게시물을 도배하지 않는다.
+          </li>
+          <li style={{ marginBottom: "10px" }}>어그로를 끌지 않는다.</li>
+          <li style={{ marginBottom: "10px" }}>
+            친구를 왕따시키지 않는다.
+          </li>
+        </ul>
+      </Stack>
+      <Stack spacing={1}>
+        <Stack
+          bgcolor="#D76464"
+          direction="row"
+          sx={{
+            color: "white",
+            borderRadius: "20px",
+            alignItems: "center",
+            width: "170px",
+            height: "30px",
+            "& .MuiIconButton-root": {
+              backgroundColor: "white",
+            },
+          }}
+        >
+          <Stack fontSize="15px" marginLeft="30%">
+            확인 했어요~!
+          </Stack>
+          <Checkbox
+            sx={{
+              color: "white",
+              "&.Mui-checked": {
+                color: "white",
+              },
+            }}
+          />
+        </Stack>
+
+        <Stack
+          bgcolor="#7AAAA7"
+          sx={{
+            cursor: "pointer",
+            color: "white",
+            borderRadius: "20px",
+            alignItems: "center",
+            width: "170px",
+            height: "30px",
+            justifyContent: "center",
+          }}
+          onClick={() => {
+            //이후 추가
+          }}
+        >
+          <Stack fontSize="15px">글쓰기</Stack>
+        </Stack>
+      </Stack></Stack>
       </Stack>
     </Modal>
   );
