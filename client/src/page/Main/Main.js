5bfa49126849b23a6ea4d1e7ec4a2b8d24dd20e7
suyeon <moa0823@naver.com>
2023-09-03 21:49:33 +0900

    #73 봉봉이 소환

--- client/src/page/Main/Main.js
+++ client/src/page/Main/Main.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { Stack } from "@mui/material";
+import bongbong from "../../image/bongbong.gif";
 
 function Main() {
-  return <Stack minHeight="100vh" height="100vh" weight="100%" />;
+  return (
+    <Stack minHeight="100vh" height="100vh" weight="100%">
+      <img width="100px" height="120px" alt="bongbong" src={bongbong} />
+    </Stack>
+  );
 }
 
 export default Main;
