d4a7cdecaf5c7244be9f0ef5eca3ae6b97a7ab2b
suyeon <moa0823@naver.com>
2023-09-03 23:39:27 +0900

    #73 배너들

--- client/src/page/Main/Main.js
+++ client/src/page/Main/Main.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Stack } from "@mui/material";
 import bongbong from "../../image/bongbong.gif";
+import banner from "../../image/banner.png";
+import Header from "../../component/header";
+import RubberBand from "react-reveal/RubberBand";
 
 function Main() {
   const mouseMove = (e) => {
@@ -16,16 +19,62 @@ function Main() {
       weight="100%"
       onMouseMove={mouseMove}
     >
+      <Header />
+
+      <Stack
+        width="100%"
+        alignItems="center"
+        gap="100px"
+        style={{ marginTop: "150px" }}
+      >
+        <RubberBand>
+          <img
+            width="900px"
+            height="250px"
+            alt="banner"
+            src={banner}
+            style={{ cursor: "pointer" }}
+          />
+        </RubberBand>
+        <RubberBand>
+          <img
+            width="900px"
+            height="250px"
+            alt="banner"
+            src={banner}
+            style={{ cursor: "pointer" }}
+          />
+        </RubberBand>
+        <RubberBand>
+          <img
+            width="900px"
+            height="250px"
+            alt="banner"
+            src={banner}
+            style={{ cursor: "pointer" }}
+          />
+        </RubberBand>
+        <RubberBand>
+          <img
+            width="900px"
+            height="250px"
+            alt="banner"
+            src={banner}
+            style={{ cursor: "pointer" }}
+          />
+        </RubberBand>
+      </Stack>
+
       <img
         width="100px"
         height="125px"
         alt="bongbong"
         src={bongbong}
         style={{
-          position: "fixed",
+          position: "absolute",
           left: `${position.x}px`,
           top: `${position.y}px`,
-          transform: "translate(-50%, 30%)",
+          transform: "translate(-50%, 0%)",
           transition: "left 1s ease-out, top 1s ease-out",
         }}
       />
