2d0f7848aeff4a2804f702ac2db69ba179971af8
suyeon <moa0823@naver.com>
2023-09-03 22:38:09 +0900

    #73 봉봉이 마우스 따라 움직이는 기능 구현

--- client/src/page/Main/Main.js
+++ client/src/page/Main/Main.js
@@ -3,9 +3,32 @@ import { Stack } from "@mui/material";
 import bongbong from "../../image/bongbong.gif";
 
 function Main() {
+  const mouseMove = (e) => {
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const [position, setPosition] = React.useState({ x: 0, y: 0 });
+
   return (
-    <Stack minHeight="100vh" height="100vh" weight="100%">
-      <img width="100px" height="120px" alt="bongbong" src={bongbong} />
+    <Stack
+      minHeight="100vh"
+      height="100vh"
+      weight="100%"
+      onMouseMove={mouseMove}
+    >
+      <img
+        width="100px"
+        height="125px"
+        alt="bongbong"
+        src={bongbong}
+        style={{
+          position: "absolute",
+          left: `${position.x}px`,
+          top: `${position.y}px`,
+
+          transform: "translate(-50%, -50%)",
+        }}
+      />
     </Stack>
   );
 }
