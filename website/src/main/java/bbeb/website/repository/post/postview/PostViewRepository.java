889d535e08031be6d0cb113f364f64b158e77cd3
jang010505 <jang010505@inu.ac.kr>
2023-09-10 17:00:36 +0900

    fix: #85 Fix delete error

--- website/src/main/java/bbeb/website/repository/post/postview/PostViewRepository.java
+++ website/src/main/java/bbeb/website/repository/post/postview/PostViewRepository.java
@@ -5,6 +5,9 @@ import bbeb.website.domain.post.Post;
 import bbeb.website.domain.post.PostView;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostViewRepository extends JpaRepository<PostView, Long> {
     PostView findByPostAndMember(Post post, Member member);
+    List<PostView> findAllByPost(Post post);
 }
