35f5f6b4825436fd6ddf21c6e0fcf72b27a1491b
jang010505 <jang010505@inu.ac.kr>
2023-09-26 21:53:26 +0900

    fix: #94 Modify emoticon

--- website/src/main/java/bbeb/website/service/CommentService.java
+++ website/src/main/java/bbeb/website/service/CommentService.java
@@ -40,7 +40,7 @@ public class CommentService {
         comment.setMember(member);
         comment.setPost(post);
         comment.setCreateDate(LocalDateTime.now());
-        comment.setUrl(dto.getUrl());
+        comment.setEmoticonNumber(dto.getEmoticonNumber());
 
         commentRepository.save(comment);
     }
@@ -62,7 +62,7 @@ public class CommentService {
         if (Objects.equals(comment.getMember().getLoginId(), loginId)){
             comment.setCommentType(Objects.equals(dto.getType(), "TEXT") ? CommentType.TEXT : CommentType.EMOTICON);
             comment.setValue(dto.getValue());
-            comment.setUrl(dto.getUrl());
+            comment.setEmoticonNumber(dto.getEmoticonNumber());
         }
         else
             throw new CustomException(ErrorCode.USER_NOT_FOUND);
