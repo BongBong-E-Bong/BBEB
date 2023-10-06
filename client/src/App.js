70908dfa1db38fe60ad6a73a945865fc8a8d8b37
P.S.J <126138033+sojeong0302@users.noreply.github.com>
2023-10-06 11:21:08 +0900

    #107 업데이트 파일 추가

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Post from "../src/page/Board/post.js";
 import RPS from "../src/page/RPS/RPS.js";
 import Choice from "../src/page/Choice/Choice.js";
 import Write from "../src/page/Write/Write.js";
+import WriteUpdate from "../src/page/Write/writeUpdate.js";
 
 function App() {
   return (
@@ -33,6 +34,7 @@ function App() {
         <Route path="/RPS" element={<RPS />} />
         <Route path="/Choice" element={<Choice />} />
         <Route path="/Write" element={<Write />} />
+        <Route path="/Write/:id" element={<WriteUpdate />} />
       </Routes>
     </Router>
   );
