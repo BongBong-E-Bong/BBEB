71c92261fe105796b1c1bfa24ac62fd03f06727e
sojeong0302 <01duqrl@naver.com>
2023-10-13 23:44:28 +0900

    #121 코드 정리

--- client/src/component/login.js
+++ client/src/component/login.js
@@ -46,14 +46,6 @@ function Login({ setOpen }) {
         setErrorMessage("아이디/비밀번호가 틀렸어요!!");
       });
   };
-
-  const REST_API_KEY='REST API KEY';
-  const REDIRECT_URI='http://localhost:3000/oauth2/redirect';
-  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-
-  const loginHandler = () => {
-    window.location.href = link;
-  };
   
   return (
     <Stack
@@ -158,7 +150,9 @@ function Login({ setOpen }) {
             justifyContent: "center",
             marginTop: "10%",
           }}
-          onClick={loginHandler}
+          onClick={{
+            // 이후 백엔드와 함께 작업
+          }}
         >
           <img src={kakaologo} alt="kakaologo icon" />
         </Stack>
