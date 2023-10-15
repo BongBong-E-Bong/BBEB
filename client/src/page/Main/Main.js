f121316fb9650f1a546e89aadc49584c775ae69d
suyeon <moa0823@naver.com>
2023-10-16 03:08:49 +0900

    #130 배너 이미지 완성

--- client/src/page/Main/Main.js
+++ client/src/page/Main/Main.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { Stack, Button } from "@mui/material";
 import bongbong from "../../image/bongbong.gif";
-import banner from "../../image/banner.png";
+import webtoon from "../../image/webtoonBanner.png";
+import profile from "../../image/profileBanner.png";
+import vote from "../../image/voteBanner.png";
+import ranking from "../../image/rankingBanner.png";
+import minigame from "../../image/minigameBanner.png";
+import board from "../../image/boardBanner.png";
 import Header from "../../component/header";
 import { useNavigate } from "react-router-dom";
 import Fade from "react-reveal/Fade";
@@ -34,12 +39,38 @@ function Main() {
       navigate: "https://comic.naver.com/challenge/list?titleId=806769",
       variant: "contained",
       openNewTab: true,
+      banner: webtoon,
     },
-    { label: "이봉이 형제 프로필", navigate: "/profile", variant: "outlined" },
-    { label: "투표하러 가기", navigate: "/choice", variant: "contained" },
-    { label: "인기 투표 순위", navigate: "/ranking", variant: "outlined" },
-    { label: "미니 게임", navigate: "/rps", variant: "contained" },
-    { label: "오봉이의 게시판", navigate: "/writelist", variant: "outlined" },
+    {
+      label: "이봉이 형제 프로필",
+      navigate: "/profile",
+      variant: "outlined",
+      banner: profile,
+    },
+    {
+      label: "투표하러 가기",
+      navigate: "/choice",
+      variant: "contained",
+      banner: vote,
+    },
+    {
+      label: "인기 투표 순위",
+      navigate: "/ranking",
+      variant: "outlined",
+      banner: ranking,
+    },
+    {
+      label: "미니 게임",
+      navigate: "/rps",
+      variant: "contained",
+      banner: minigame,
+    },
+    {
+      label: "오봉이의 게시판",
+      navigate: "/writelist",
+      variant: "outlined",
+      banner: board,
+    },
   ];
 
   return (
@@ -56,13 +87,17 @@ function Main() {
         gap="100px"
         style={{ marginTop: "150px" }}
       >
+        <Stack>
+          *회원 가입이 번거로우시다면 우측 상단에서 로그인을 누른 후 아이디,
+          비밀번호에 string을 입력하고 이용해주세요!
+        </Stack>
         {buttonsData.map((button, i) => (
           <Fade>
             <img
               width="900px"
               height="250px"
               alt="banner"
-              src={banner}
+              src={button.banner}
               style={{ cursor: "pointer" }}
               onClick={() => {
                 if (button.openNewTab) {
