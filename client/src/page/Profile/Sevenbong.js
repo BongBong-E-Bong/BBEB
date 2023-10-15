dc47f6c66dcdbf568df9e245c297a2b9e2c8d9b5
suyeon <moa0823@naver.com>
2023-10-16 01:43:41 +0900

    #115 칠봉 완성

--- client/src/page/Profile/Sevenbong.js
+++ client/src/page/Profile/Sevenbong.js
@@ -10,16 +10,12 @@ function Sevenbong() {
   const commentRef = React.useRef(null);
   const profileRef = React.useRef(null);
   const tmiRef = React.useRef(null);
-  const storyRef = React.useRef(null);
 
   const commentClick = () => {
     commentRef.current.scrollIntoView({ behavior: "smooth" });
   };
   const profileClick = () => {
     profileRef.current.scrollIntoView({ behavior: "smooth" });
-  };
-  const storyClick = () => {
-    storyRef.current.scrollIntoView({ behavior: "smooth" });
   };
   const tmiClick = () => {
     tmiRef.current.scrollIntoView({ behavior: "smooth" });
@@ -81,11 +77,6 @@ function Sevenbong() {
         </Stack>
 
         <Stack width="80%" height="fit-content" margin="20vh 10% 0 10%">
-          <Stack ref={storyRef} />
-          <Stack style={{ fontSize: "27px" }} marginBottom="10vh">
-            🔎칠봉이가 이상한 말투를 쓰는 이유
-          </Stack>
-
           <Stack ref={tmiRef} />
           <Stack style={{ fontSize: "27px" }} margin="20vh 0 3vh 0">
             ✨칠봉이 tmi
@@ -93,17 +84,15 @@ function Sevenbong() {
           <Stack style={{ fontSize: "20px" }} margin="0 5% 20vh 5%">
             꿈 : 개발자
             <br />
-            싫어하는 것 : 알파벳 B, 숫자 3 (눕히면 엉덩이 모양이라서)
+            좋아하는 것 : 빵, 형아들, 작고 귀여운 동물
             <br />
-            좋아하는 것 : 식물
+            싫어하는 것 : 귀신, 주사 등 평범한 아이들이 싫어하는 것
             <br />
             교우관계 : 원만(중학교~)
             <br />
-            성격 : 예의바르고 배려심이 깊으며 소심하지만 할 말은 하는 똑부러진
-            성격이다. (다만 말한 뒤 눈치를 보는 편)
+            성격 : 매우 밝으며 친화력이 좋고 마음씨도 착하다.
             <br />
-            종교 : 무교 (어릴 적 엉덩이가 작아지게 해달라고 간절히 빌었지만
-            이루어지지 않아 신을 매우 불신한다.)
+            특징 : 언제나 활짝 웃고 있다.
           </Stack>
         </Stack>
         <Stack ref={commentRef}></Stack>
@@ -122,11 +111,6 @@ function Sevenbong() {
         gap="10px"
       >
         <Stack onClick={profileClick}>소개</Stack>
-
-        <Stack onClick={storyClick}>
-          🔎칠봉이가 이상한 말투를 <br />
-          쓰는 이유
-        </Stack>
 
         <Stack onClick={tmiClick}>✨칠봉이 tmi</Stack>
         <Stack onClick={commentClick}>댓글</Stack>
